94815229232d61ed89a50d9f9a399867e513f00f
Stalin <sta_vla411@hotmail.com>
2023-01-29 00:04:30 -0500

    add hover button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 
 const Btn = styled.a`
   color: ${(props) => props.theme.darckPurple};
-  padding: 0.5rem 1rem;
+  padding: 0.75rem 1.5rem;
   background: linear-gradient(
     90deg,
     ${(props) => props.theme.mLightPurple} 0%,
@@ -12,6 +12,11 @@ const Btn = styled.a`
   border-radius: 40px;
   font-weight: bold;
   font-size: ${(props) => props.theme.xsFont};
+  &:hover {
+    color: ${(props) => props.theme.white};
+    background: none;
+    box-shadow: inset 0 0 0 3px ${(props) => props.theme.white};
+  }
 `
 function Button({ href, text }) {
   return <Btn href={href}>{text.toUpperCase()}</Btn>
