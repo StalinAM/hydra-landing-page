c000673777b1e462b656109467ebad51b0ca2854
Stalin <sta_vla411@hotmail.com>
2023-01-29 18:22:05 -0500

    add homme and semantic html

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { ThemeProvider } from 'styled-components'
 import Header from './components/Header'
+import Home from './components/Home'
 import GlobalStyles from './style/GlobalStyles'
 import { darck } from './style/Theme'
 
@@ -9,6 +10,9 @@ function App() {
       <GlobalStyles />
       <ThemeProvider theme={darck}>
         <Header />
+        <main>
+          <Home />
+        </main>
       </ThemeProvider>
     </>
   )
