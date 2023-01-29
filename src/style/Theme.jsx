0317727d5eb82828f9615852350abf96dfd3999e
Stalin <sta_vla411@hotmail.com>
2023-01-28 20:36:22 -0500

    add theme

--- src/style/Theme.jsx
+++ src/style/Theme.jsx
@@ -1,1 +1,16 @@
-export const darck = {}
+export const darck = {
+  // Colors
+  white: '#FFFFFF',
+  lightPurple: '#C0B7E8',
+  mLightPurple: '#8176AF',
+  xLightPurple: '#403A5F',
+  darkPurple: '#211E2E',
+  // Font size
+  numSize: '4 rem',
+  xxlFont: '2.875 rem',
+  xlFont: '2.25 rem',
+  lFont: '1.5 rem',
+  mFont: '1 rem',
+  sFont: '0.875 rem',
+  xsFont: '0.75 rem'
+}
