0cb77d6ebef9b63cf6fbfaff8062695f87b167cd
Stalin <sta_vla411@hotmail.com>
2023-02-02 21:32:01 -0500

    add effect3d component

---
+++ src/components/Item3D.jsx
@@ -0,0 +1,16 @@
+const coordTransform = (event, setCoordTransformS, halfWidth, halfHeight) => {
+  const { offsetX, offsetY } = event.nativeEvent
+  const rotationY = ((offsetX - halfWidth) / halfWidth) * 14
+  const rotationX = ((offsetY + halfHeight) / halfHeight) * 14
+  setCoordTransformS({
+    transitionDuration: '300ms',
+    transform: `translate3d(0, 0, 120px) rotateX(${rotationX}deg) rotateY(${rotationY}deg)`,
+    transitionTimingFunction: 'ease-out'
+  })
+}
+const restoreTransform = (setCoordTransformS) => {
+  setCoordTransformS({
+    transform: 'translate3d(0, 0, 0) rotateX(0deg) rotateY(0deg)'
+  })
+}
+export { coordTransform, restoreTransform }
