13ed8ac11efad7aa3be411c4c7f1fb4d25573305
Stalin <sta_vla411@hotmail.com>
2023-02-06 21:28:21 -0500

    in process

--- src/components/SlideShow.jsx
+++ src/components/SlideShow.jsx
@@ -5,7 +5,7 @@ import styled from 'styled-components'
 const Slideshow = ({ children, controles = true }) => {
   const slideshow = useRef(null)
 
-  const siguiente = useCallback()
+  const siguiente = useCallback(() => {})
 
   const anterior = () => {
     if (slideshow.current.children.length > 0) {
